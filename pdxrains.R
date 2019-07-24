a57a88dfc5d373a05e5633c791f1bf8a8063b39c
GrahamPTaylor <grtails@gmail.com>
2019-07-24 11:47:00 -0700

    rainplotupdate
    Added more labels, and refined color scale.

--- pdxrains.R
+++ pdxrains.R
@@ -125,35 +125,66 @@ mds <- c(31, #jan
          31+28+31+30+31+30+31+31+30+31+30, #nov
          31+28+31+30+31+30+31+31+30+31+30+31) #dec
 
-q <- ggplot(drd, aes(newDay, Year, fill =  Rainday)) + 
+q <- ggplot(drd, aes(newDay, Year, fill =  Rain)) + 
   geom_tile() + 
   #facet_grid(Year~Month) + 
-  scale_fill_continuous(low="light blue", high="blue", guide = "colorbar") +
-  labs(title = "PDX Daily Rainfall 1980 - 2017",
+  scale_fill_continuous(low="slategray2", high="purple4", guide = "colorbar") +
+  labs(title = "PDX Daily Rainfall 1982 - 2017",
        #subtitle = "Data up to April 2018",
        x = "Day of Year",
        y = "Year",
        fill = "Rain (in)") +
-  #theme(axis.text.x = element_blank()) + 
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
         panel.background = element_blank(), 
-        axis.line = element_blank())+
+        axis.line = element_blank(),
+        axis.text=element_text(size=10))+
+  theme(plot.title = element_text(hjust = 0.5, size = 12))+
   scale_x_continuous(limits = c(0,366), breaks = seq(0,365,30)) +
   scale_y_continuous(limits = c(1981,2018), breaks = seq(1982,2017,1)) +
-  geom_segment(aes(x = mds[1], y = 1981.5, xend = mds[1], yend = 2017.5))+
-  geom_segment(aes(x = mds[2], y = 1981.5, xend = mds[2], yend = 2017.5))+
-  geom_segment(aes(x = mds[3], y = 1981.5, xend = mds[3], yend = 2017.5))+
-  geom_segment(aes(x = mds[4], y = 1981.5, xend = mds[4], yend = 2017.5))+
-  geom_segment(aes(x = mds[5], y = 1981.5, xend = mds[5], yend = 2017.5))+
-  geom_segment(aes(x = mds[6], y = 1981.5, xend = mds[6], yend = 2017.5))+
-  geom_segment(aes(x = mds[7], y = 1981.5, xend = mds[7], yend = 2017.5))+
-  geom_segment(aes(x = mds[8], y = 1981.5, xend = mds[8], yend = 2017.5))+
-  geom_segment(aes(x = mds[9], y = 1981.5, xend = mds[9], yend = 2017.5))+
-  geom_segment(aes(x = mds[10], y = 1981.5, xend = mds[10], yend = 2017.5))+
-  geom_segment(aes(x = mds[11], y = 1981.5, xend = mds[11], yend = 2017.5))+
-  geom_segment(aes(x = mds[12], y = 1981.5, xend = mds[12], yend = 2017.5))
-head(drd)
+  
+  # lines indicating month breaks
+  
+  geom_segment(aes(x = mds[1], y = 1981.5, xend = mds[1], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[2], y = 1981.5, xend = mds[2], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[3], y = 1981.5, xend = mds[3], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[4], y = 1981.5, xend = mds[4], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[5], y = 1981.5, xend = mds[5], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[6], y = 1981.5, xend = mds[6], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[7], y = 1981.5, xend = mds[7], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[8], y = 1981.5, xend = mds[8], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[9], y = 1981.5, xend = mds[9], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[10], y = 1981.5, xend = mds[10], yend = 2017.5), color = "grey")+
+  geom_segment(aes(x = mds[11], y = 1981.5, xend = mds[11], yend = 2017.5), color = "grey")+
+  
+  # lines indicating seasonal breaks
+  # spring
+#  geom_segment(aes(x = mds[2] + 20, y = 1981.5, xend = mds[2] + 20, yend = 2017.5),
+#               size = 0.8, color = "dark green", alpha = 0.7) +
+  #summer
+#  geom_segment(aes(x = mds[5] + 20, y = 1981.5, xend = mds[5] + 20, yend = 2017.5),
+#               size = 0.8, color = "dark green", alpha = 0.7) +
+  #fall
+#  geom_segment(aes(x = mds[8] + 20, y = 1981.5, xend = mds[8] + 20, yend = 2017.5),
+#               size = 0.8, color = "dark green", alpha = 0.7) +
+  #winter
+#  geom_segment(aes(x = mds[11] + 20, y = 1981.5, xend = mds[11] + 20, yend = 2017.5),
+#               size = 0.8, color = "dark green", alpha = 0.7) +
+  
+  # month labels
+  
+  annotate("text", x = mds[1]/2, y = 2018, label = "Jan", size = 4)+
+  annotate("text", x = mds[2]-(mds[2]-mds[1])/2, y = 2018, label = "Feb", size = 4)+
+  annotate("text", x = mds[3]-(mds[3]-mds[2])/2, y = 2018, label = "Mar", size = 4)+
+  annotate("text", x = mds[4]-(mds[4]-mds[3])/2, y = 2018, label = "Apr", size = 4)+
+  annotate("text", x = mds[5]-(mds[5]-mds[4])/2, y = 2018, label = "May", size = 4)+
+  annotate("text", x = mds[6]-(mds[6]-mds[5])/2, y = 2018, label = "Jun", size = 4)+
+  annotate("text", x = mds[7]-(mds[7]-mds[6])/2, y = 2018, label = "Jul", size = 4)+
+  annotate("text", x = mds[8]-(mds[8]-mds[7])/2, y = 2018, label = "Aug", size = 4)+
+  annotate("text", x = mds[9]-(mds[9]-mds[8])/2, y = 2018, label = "Sep", size = 4)+
+  annotate("text", x = mds[10]-(mds[10]-mds[9])/2, y = 2018, label = "Oct", size = 4)+
+  annotate("text", x = mds[11]-(mds[11]-mds[10])/2, y = 2018, label = "Nov", size = 4)+
+  annotate("text", x = mds[12]-(mds[12]-mds[11])/2, y = 2018, label = "Dec", size = 4) 
 
 q
 
